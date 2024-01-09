c3b31f7113d527ba2fd06042449e77b86ce40daf
Bryant Mouton <bryantsmp01@gmail.com>
2024-01-10 12:36:41 +1300

    added verifications to fields+making cookie for variable

--- School_Website/JavaScript/userPasswordReset.js
+++ School_Website/JavaScript/userPasswordReset.js
@@ -1,12 +1,23 @@
 document.getElementById("submitButton").addEventListener("click", async function () {
   const userEmail = document.getElementById("email").value;
+   // Set a cookie with the user's email
+   document.cookie = `userEmail=${userEmail}; expires=${getCookieExpiration()}; path=/`;
+
 // Check if the user entered a valid Gmail address
+/* Need to check if the email entered exists in the database */
 if (!isValidEmail(userEmail)) {
     alert("Please enter a valid Gmail address.");
     return; // Stop the function execution if the email is not valid
 }
   try {
-      // Make a fetch request to your server to initiate the password reset process
+    // Make fetch request to server to check if gmail exist in the database
+    const emailExistRespone = await fetch(`http://localhost:5000/api/user/gmailExist?gmail=${encodeURIComponent(userEmail)}`);
+    const emailExistResult = await emailExistRespone.json();
+    // Login to check if email exists
+    if(emailExistResult.status ==='success' && emailExistResult.gmailExists){
+      //Success continue to run code
+     
+      // Make fetch request to server to initiate the password reset process
       const response = await fetch("http://localhost:5000/api/user/resetPassword", {
           method: 'POST',
           headers: {
@@ -18,7 +29,7 @@ if (!isValidEmail(userEmail)) {
 
       if (response.ok) {
         console.log('Password reset initiated successfully!');
-
+        
         // Handle success. Showing a success message to the user
         alert("Check your email for the verification code!");
     
@@ -28,6 +39,10 @@ if (!isValidEmail(userEmail)) {
         console.error('Failed to initiate password reset:', response.statusText);
         // Handle failure, e.g., show an error message to the user
       }
+    }else{
+      alert("Enter a valid gmail.");// change error message
+    }
+
   } catch (error) {
       console.error('Error:', error);
       // Handle other errors, e.g., network issues
@@ -43,3 +58,9 @@ function isValidEmail(email) {
 /* he isValidEmail function checks if the entered email is a valid Gmail address using a simple regular expression.
 If the entered email is not valid, it displays an alert to the user.
 If the email is valid, it proceeds with the password reset initiation logic. */
+// Function to get the expiration time for the cookie (e.g., 1 hour from now)
+function getCookieExpiration() {
+  const expirationDate = new Date();
+  expirationDate.setTime(expirationDate.getTime() + 60 * 60 * 1000); // 1 hour
+  return expirationDate.toUTCString();
+}
