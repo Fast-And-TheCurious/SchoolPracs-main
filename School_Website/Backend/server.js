dd85101d7124081a099587dd70cce9f26214bedd
Bryant Mouton <bryantsmp01@gmail.com>
2024-07-02 15:45:00 +1300

    self note added

--- School_Website/Backend/server.js
+++ School_Website/Backend/server.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const app = express();
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 5000; // change port? 
 const cors = require("cors");
 const cookieParser = require('cookie-parser');
 const cookie=require('cookie');
