baa53c08131bed2cf7a2927f046799d593c85302
Bryant Mouton <bryantsmp01@gmail.com>
2024-07-23 13:54:57 +1300

    added updating of lessons completed by userID

--- School_Website/Backend/lessonManager.js
+++ School_Website/Backend/lessonManager.js
@@ -1,54 +1,3 @@
-/* const connection = require('./database'); 
-function getLessonData(callback) {
-  connection.query('SELECT * FROM lessons', (error, results) => {
-    if (error) {
-      console.error('Error retrieving data from the database: ' + error.message);
-      return callback(error);
-    }
-    callback(null, results);
-  });
-}
-function formatData(results) {
-  const lessonData = {
-    lessons: results.map((row) => ({
-      id: row.id,
-      unit: row.unit,
-      unitTitle: row.unitTitle,
-      note: row.note,
-      aboutUnit: row.aboutUnit,
-      unitlessonContent: {
-        uniquelessonTitles: {
-          lesson1Title: row.lesson1Title,
-          lesson2Title: row.lesson2Title,
-          lesson3Title: row.lesson3Title,
-          lesson4Title: row.lesson4Title,
-        },
-        lessonLink: {
-          firstLink: row.firstLink,
-          secondLink: row.secondLink,
-          thirdLink: row.thirdLink,
-          fourthLink: row.fourthLink,
-        },
-        youTubeVideos: {
-          lesson1Video: row.lesson1Video,
-          lesson2Video: row.lesson2Video,
-          lesson3Video: row.lesson3Video,
-          lesson4Video: row.lsson4Video,
-        },
-      },
-    })),
-  };
-  return lessonData;
-}
-
-getLessonData((error, results) => {
-  if (error) {
-    return;
-  }
-  const lessonData = formatData(results);
-  console.log(JSON.stringify(lessonData, null, 2));
-}); */
-
 /* Lesson */
 const { select } = require("./database");
 
@@ -123,6 +72,15 @@ class lessonManager {
       console.error("An error occurred while fetching lessons by units:", error);
       return { error: "An error occurred while processing the request", statusCode: 500 };
     }
+  }
+  async updateLessonsCompleted(){
+  try{
+    const query = `UPDATE users SET lessons_completed = lessons_completed+1 WHERE id = ?`;
+    await update(query, [userID]);
+    return "Update user lessons completed successful";   
+  }catch(error){
+    return error;
+  }
   }
 }
 
